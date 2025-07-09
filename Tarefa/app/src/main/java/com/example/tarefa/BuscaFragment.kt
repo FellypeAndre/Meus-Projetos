a94dc348cf99c64b15ef4e610ed24f22445c2ada
André Fellype <andre.fellype39@gmail.com>
2025-07-09 16:56:01 -0300

    Tarefa.app

--- Tarefa/app/src/main/java/com/example/tarefa/BuscaFragment.kt
+++ Tarefa/app/src/main/java/com/example/tarefa/BuscaFragment.kt
@@ -1,26 +1,21 @@
 package com.example.tarefa
 
-import android.app.Fragment
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment  // Certo!
 
 class BuscaFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
+        super.onCreate(savedInstanceState) // faltava isso
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_busca, container, false)
     }
-
-
 }
--- Tarefa/app/src/main/java/com/example/tarefa/MainActivity.kt
+++ Tarefa/app/src/main/java/com/example/tarefa/MainActivity.kt
@@ -1,47 +1,92 @@
 package com.example.tarefa
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.tarefa.ui.theme.TarefaTheme
+import android.view.MenuItem
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener, BottomNavigationView.OnNavigationItemSelectedListener  {
+//    private lateinit var buttonForm: Button
+//    private lateinit var buttonHome: Button
+//    private lateinit var buttonBusca: Button
+
+    private lateinit var homeFragment: HomeFragment
+    private lateinit var buscaFragment: BuscaFragment
+    private lateinit var formFragment: FormFragment
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            TarefaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
+//        buttonHome = findViewById(R.id.button_home)
+//        buttonHome.setOnClickListener(this)
+//
+//
+//        buttonForm = findViewById(R.id.button_form)
+//        buttonForm.setOnClickListener(this)
+//
+//        buttonBusca = findViewById(R.id.button_busca)
+//        buttonBusca.setOnClickListener(this)
+
+        homeFragment = HomeFragment()
+        formFragment = FormFragment()
+        buscaFragment = BuscaFragment()
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+
+        setFragment(homeFragment)
+
     }
+
+    private fun setFragment(fragment: Fragment){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_fragments, fragment)
+        fragmentTransaction.commit()
+    }
+
+    override fun onClick(v: View) {
+//        when (v.id){
+//            R.id.button_home -> {
+//                setFragment(homeFragment)
+//
+//            }
+//            R.id.button_form -> {
+//                setFragment(formFragment)
+//
+//            }
+//            R.id.button_busca -> {
+//                setFragment(buscaFragment)
+//
+//            }
+//        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+       when (item.itemId){
+           R.id.menu_home -> {
+               setFragment(homeFragment)
+
+           }
+           R.id.menu_form -> {
+               setFragment(formFragment)
+
+           }
+           R.id.menu_busca -> {
+               setFragment(buscaFragment)
+
+           }
+
+       }
+        return true
+
+    }
+
+
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TarefaTheme {
-        Greeting("Android")
-    }
-}
